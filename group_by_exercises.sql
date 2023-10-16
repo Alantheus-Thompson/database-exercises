9622cc793fdcb007664323ee4cb0f9f56f566769
Alantheus-Thompson <Alantheus.Thompson@gmail.com>
2023-10-16 14:24:52 -0500

    group exercises completed

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -22,7 +22,7 @@ select distinct last_name, count(last_name) # used emp_no as well in case emp ha
 		where last_name like 'E%E' -- WHERE used with count() doesn't work; purpose of group is not
         -- just that it is often used with aggregate functions but seems to be it has to be used with
         -- with aggregate functions.
-			group by last_name;
+			group by last_name; 
         
 -- ANSWER: There are 5 unique last names totaling 899 counts fairly equally divided (likely within 1 SD)
 
@@ -77,11 +77,10 @@ date_format(birth_date,'%m%y'))) as username, count(*) as unique_username_count
 -- of times a username shows up? Bonus: How many duplicate usernames are there?
 
 Select distinct lower(concat(substr(first_name, 1, 1),substr(last_name,1,4),'_', 
-date_format(birth_date,'%m%y'))) as username, count(*) as duplicate_count, sum(duplicate_count)
-as total_duplicate_count
+date_format(birth_date,'%m%y'))) as username, count(*) as duplicate_count
 -- When I tried to call
 -- the alias username without the prior function it didn't work.  Naming an alias only works in 
--- same query line.  Could create sub-query which is probably beneficial to start practicing for
+-- same query line?  Could create sub-query which is probably beneficial to start practicing for
 -- larger data sets.
 	from employees
 		group by username
@@ -90,6 +89,8 @@ as total_duplicate_count
 
 
 -- ANSWER: Yes there are.  Highest # of time a username shows up is 6.  
+## playing around: could not run when select calls out column in select function and not in group
+## by. 
 
 -- #9 BONUS: 
 
@@ -119,3 +120,72 @@ FROM (
 
 
 -- ANSWER: 27403
+
+-- Bonuses:
+-- #1 Determine the historic average salary for each employee. When you hear, read, or think 
+-- "for each" with regard to SQL, you'll probably be grouping by that exact column.
+
+show tables;
+select emp_no, avg(salary) as historical_avg
+	from salaries
+    group by emp_no;
+
+-- ANSWER: See code for answer
+
+-- #2 Using the dept_emp table, count how many current employees work in each department. The 
+-- query result should show 9 rows, one for each department and the employee count.
+
+select dept_no, count(*) as employees_in_each_department
+	from dept_emp
+    group by dept_no;
+    
+-- ANSWER: Run code above
+
+-- #3 Determine how many different salaries each employee has had. This includes both historic 
+-- and current.
+
+select distinct emp_no, count(emp_no) as num_of_diff_salaries
+	from salaries
+		group by emp_no;
+
+-- ANSWER: Run code above.
+
+-- #4 Find the maximum salary for each employee.
+
+select distinct emp_no, max(salary)
+	from salaries
+    group by emp_no;
+
+-- ANSWER: see above code
+
+-- #5 Find the minimum salary for each employee.
+
+select distinct emp_no, min(salary)
+	from salaries
+		group by emp_no;
+
+-- #6 Find the standard deviation of salaries for each employee.
+
+select emp_no, stddev(salary) 
+	from salaries
+     group by emp_no;
+
+-- ANSWER: see above
+
+-- #7 Find the max salary for each employee where that max salary is greater than $150,000.
+
+select emp_no, salary
+	from salaries
+		where salary > 150000
+			group by emp_no, salary;
+
+-- ANSWER: see above
+
+-- #8 Find the average salary for each employee where that average salary is between $80k and 
+-- $90k.
+
+select emp_no, avg(salary) as average_salary
+	from salaries
+        group by emp_no
+			having average_salary between 80000 and 90000;
+       
